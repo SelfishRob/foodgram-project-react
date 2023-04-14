b7231bf70c9b1b73181fb53dc0dede0f4323bd33
Robert Khafizov <robertkhafizov17@gmail.com>
2023-04-14 17:57:47 +0300

    add apps users, settings abstract user

---
+++ backend/api_foodgram/users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from users.validators import UnicodeUsernameValidator
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=254, unique=True)
+    username = models.CharField(max_length=150, unique=True,
+                                validators=(UnicodeUsernameValidator,))
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
