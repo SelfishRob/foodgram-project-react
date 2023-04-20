2f6e5ab387f3606c4771579525d1b74e45f10ce2
Robert Khafizov <robertkhafizov17@gmail.com>
2023-04-20 16:23:15 +0300

    Базово рабочие сериализаторы Тег и Ингредиент

--- backend/api_foodgram/api/urls.py
+++ backend/api_foodgram/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import TagViewSet
+from api.views import IngredientViewSet, TagViewSet
 
 router_v1 = DefaultRouter()
 
-router_v1.register('tag', TagViewSet, basename='tag')
+router_v1.register('tags', TagViewSet, basename='tags')
+router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
